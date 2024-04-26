1dabd971be913c5038c6c332804a03f656bbf10a
Michael Mercier <michael.mercier@ryax.tech>
2024-04-26 11:43:52 +0200

    Add result anaysis script and all heuristics in run all

--- bebida/scripts/run-all.sh
+++ bebida/scripts/run-all.sh
@@ -10,7 +10,7 @@ SCRIPT_PATH=$(dirname ${BASH_SOURCE[0]})
 
 for iteration in $(seq 30)
 do
-    for heuristic in punch none deadline nohpc refill
+    for heuristic in punch none deadline nohpc refill annotated
     do
         $SCRIPT_PATH/run-hpc-workload.sh 288 $RESULT_DIR $SCRIPT_PATH/spark-pi.yaml $heuristic 4
     done
